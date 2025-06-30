a683b435928942f64a39f687eb663e3c5270b8ed
ArdiansyahR1861 <220711861@students.uajy.ac.id>
2025-06-30 20:08:07 +0700

    db adjust

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,7 +1,9 @@
+import 'dotenv/config';
 import mysql from 'mysql2/promise';
 
 const pool = mysql.createPool({
   host: process.env.DATABASE_HOST,
+  port: process.env.DATABASE_PORT,
   user: process.env.DATABASE_USER,
   password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE_NAME,
